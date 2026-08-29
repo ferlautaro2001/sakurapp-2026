4062a1b1a3e8861763176d6449d286564ab91bcb
Lautaro Fernandez Di Bella <ferlautaro.utn.2001@gmail.com>
2026-08-29 00:34:53 -0300

    feat(backend): configure firebase data connect cloud sql schema

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: "tu-proyecto-firebase",
+    appId: "1:000000000000:web:0000000000000000000000",
+    storageBucket: "tu-proyecto.firebasestorage.app",
+    apiKey: "AIzaSy_TU_API_KEY_AQUI",
+    authDomain: "tu-proyecto.firebaseapp.com",
+    messagingSenderId: "000000000000",
+  },
+  dataConnect: {
+    service: "sakurapp",
+    location: "us-east4",
+    connector: "example",
+  },
+  defaultPassword: "Sakura.2026",
+};
+
